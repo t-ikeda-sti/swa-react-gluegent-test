668ab9436b51fd0a697b35fb0841fd34df54c1f7
TORU IKEDA <t-ikeda@sios.com>
2024-07-23 21:44:22 +0900

    add function call

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ async function getUserInfo() {
   return clientPrincipal;
 }
 
+async function getUser() {
+  const response = await fetch('/api/HttpTrigger1');
+  const payload = await response.json();
+  const { clientPrincipal } = payload;
+  return clientPrincipal;
+}
+
+
 function App() {
   const value = "World";
   (async () => console.log(await getUserInfo()))();
+  (async () => console.log(await getUser()))();
   return <div>Hello {value}</div>;
 }
 
